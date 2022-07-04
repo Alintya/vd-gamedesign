cb8cfe2cba3d1dfec186984db8b7b5a2e9fe7bd0
Jannis S <alintya.lol@gmail.com>
2022-07-04 04:59:19 +0200

    use new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,52 +1,79 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed = 5f;
-
+    public PlayerInputActions PlayerControls;
+    
     
     private Camera _cam;
   
     private Rigidbody _rb;
-    private Vector2 _movement;
-    private Vector2 _mousePos;
+    private Vector2 _targetPosition;
+    private Quaternion _targetRotation;
+
+    private InputAction _moveAction;
+    private InputAction _lookAction;
+    private InputAction _fireAction;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _cam = Camera.main;
+
+        PlayerControls = new PlayerInputActions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _movement.x = Input.GetAxisRaw("Horizontal");
-        _movement.y = Input.GetAxisRaw("Vertical");
+        _targetPosition = _moveAction.ReadValue<Vector2>();
+        var screenPos = _lookAction.ReadValue<Vector2>();
 
-        //_mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
-
-        var playerPlane = new Plane(Vector3.up, transform.position);
-        var ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        var playerPlane = new Plane(Vector3.up, transform.position);  
+        var ray = _cam.ScreenPointToRay(screenPos);
 
         float hitDist;
 
         if (playerPlane.Raycast(ray, out hitDist))
         {
             var hitPoint = ray.GetPoint(hitDist);
-            var targetRotation = Quaternion.LookRotation(hitPoint - transform.position);
-            targetRotation.x = 0;
-            targetRotation.z = 0;
-            _rb.rotation = Quaternion.Slerp(_rb.rotation, targetRotation, 7f * Time.deltaTime);
+            _targetRotation = Quaternion.LookRotation(hitPoint - transform.position);
+            _targetRotation.x = 0;
+            _targetRotation.z = 0;
         }
     }
     
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        _rb.MovePosition(_rb.position + new Vector3(_movement.x, 0, _movement.y) * MoveSpeed * Time.fixedDeltaTime);
-        
-        //Vector2 lookDir = _mousePos - new Vector2(_rb.position.x, _rb.position.y);
-        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        //_rb.rotation = Quaternion.Euler(0, angle, 0);
+        _rb.MovePosition(_rb.position + MoveSpeed * Time.fixedDeltaTime * new Vector3(_targetPosition.x, 0, _targetPosition.y));
+        _rb.rotation = Quaternion.Slerp(_rb.rotation, _targetRotation, 7f * Time.fixedDeltaTime);
+    }
+
+    private void Fire(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Fire");
+    }
+
+    void OnEnable()
+    {
+        _moveAction = PlayerControls.Player.Move;
+        _moveAction.Enable();
+
+        _lookAction = PlayerControls.Player.Look;
+        _lookAction.Enable();
+
+        _fireAction = PlayerControls.Player.Fire;
+        _fireAction.Enable();
+
+        _fireAction.performed += Fire;
+    }
+
+    void OnDisable()
+    {
+        _moveAction.Disable();
+        _lookAction.Disable();
+        _fireAction.Disable();
     }
 }
